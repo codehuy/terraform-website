179a606016a232b0d746db7c66638a4fba6f4e35
Huy <huytran3052@gmail.com>
2024-10-20 18:02:55 -0400

    Added route53 within terraform

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -16,9 +16,10 @@ resource "aws_acm_certificate" "ssl_certificate" {
 
 # Create a DNS record in Route 53 to validate the ACM certificate
 resource "aws_route53_record" "dns_validation" {
-  name    = aws_acm_certificate.ssl_certificate.domain_validation_options[0].resource_record_name
-  records = [aws_acm_certificate.ssl_certificate.domain_validation_options[0].resource_record_value]
-  type    = aws_acm_certificate.ssl_certificate.domain_validation_options[0].resource_record_type
+  allow_overwrite = true
+  name    = tolist(aws_acm_certificate.ssl_certificate.domain_validation_options)[0].resource_record_name
+  records = [tolist(aws_acm_certificate.ssl_certificate.domain_validation_options)[0].resource_record_value]
+  type    = tolist(aws_acm_certificate.ssl_certificate.domain_validation_options)[0].resource_record_type
   zone_id = data.aws_route53_zone.dns_zone.zone_id
   ttl     = 300
 }
